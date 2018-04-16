920dbc6a1de0a8b0715a82d331f4dc9fe2b39611
James Donald <jdonald.x@gmail.com>
2018-04-16 01:08:27 -0700

    Disable new yellow componentWillMount() lifecycle warnings

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import {
 
 import Finance from './Finance';
 
+console.disableYellowBox = true;
 StatusBar.setBarStyle('light-content', true);
 
 AppRegistry.registerComponent('Finance', () => Finance);
